f27331a4543b32b1c4aafe0be5123685f88519db
buchraa <ndiayebouchraa@gmail.com>
2020-12-06 03:29:20 +0100

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/biblio-tech-front'));
+ngApp.use(express.static('./dist/biblioTech-front'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, './dist/biblio-tech-front/index.html'));
+    response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
