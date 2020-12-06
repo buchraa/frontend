21da583c1140c10fce798a9527ad08e09f0a0f48
buchraa <ndiayebouchraa@gmail.com>
2020-12-06 03:17:17 +0100

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/maktabatoul-mouridiya'));
+ngApp.use(express.static('./dist/biblio-tech-front'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, './dist/maktabatoul-mouridiya/index.html'));
+    response.sendFile(path.join(__dirname, './dist/biblio-tech-front/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
--- src/app/ui/main-menu/server.js
+++ src/app/ui/main-menu/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/maktabatoul-mouridiya'));
+ngApp.use(express.static('./dist/biblioTech-front'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, './dist/maktabatoul-mouridiya/index.html'));
+    response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
