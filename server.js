5d9020289c0562f70540a8c26b11076a53bc7c5f
buchraa <ndiayebouchraa@gmail.com>
2021-04-16 01:55:08 +0200

    edit server

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/biblioTech-front'));
+ngApp.get("/ngsw-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "ngsw-worker.js"));
+});
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
 
-ngApp.get("/ngsw-worker.js", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "ngsw-worker.js"));
-  });
+
 
 ngApp.listen(process.env.PORT || 8080);
 
