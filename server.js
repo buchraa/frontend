38f63646f2021a1ec32aff93a013bb7eabbd3111
buchraa <ndiayebouchraa@gmail.com>
2021-04-16 01:45:56 +0200

    edit server

--- server.js
+++ server.js
@@ -5,5 +5,10 @@ ngApp.use(express.static('./dist/biblioTech-front'));
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
+
+ngApp.get("/ngsw-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "ngsw-worker.js"));
+  });
+
 ngApp.listen(process.env.PORT || 8080);
 
