292f9fd1bf744726fa9c3f3dbfee3f5f02108573
buchraa <ndiayebouchraa@gmail.com>
2021-04-16 12:02:10 +0200

    fixed sw

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/biblioTech-front'));
-ngApp.get("/maktabatoul-mouridiya.herokuapp.com/ngsw-worker.js", (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/biblioTech-front/ngsw-worker.js'));
-});
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
