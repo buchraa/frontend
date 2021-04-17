d21d3fccbd819b9dbe95588f3d22b56c3c06e40f
buchraa <ndiayebouchraa@gmail.com>
2021-04-17 13:36:27 +0200

    sw get registered

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/biblioTech-front'));
-
-
-
-/*
-ngApp.get('/ngsw-worker.js', function (request, response) {
-    response.sendFile(path.join(__dirname, './dist/biblioTech-front/ngsw-worker.js'));
-});*/
-
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, './dist/biblioTech-front/index.html'));
 });
