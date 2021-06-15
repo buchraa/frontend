6f4122085be5f569e1d8e6d13969967f2c8013b6
buchraa <ndiayebouchraa@gmail.com>
2021-06-15 23:25:29 +0200

    fix font size

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://vps-8aea0a63.vps.ovh.net:8085'
-  //'https://bibliotechmouride.herokuapp.com'
+  apiUrl: 'https://bibliotechmouride.herokuapp.com'
+  //'http://vps-8aea0a63.vps.ovh.net:8085'
   
   //'https://bibliotechmouride.herokuapp.com' //'http://vps-2cf1715a.vps.ovh.net:8085'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://vps-8aea0a63.vps.ovh.net:8085'
-  //'https://bibliotechmouride.herokuapp.com' //
+  apiUrl: 'https://bibliotechmouride.herokuapp.com' //'http://vps-8aea0a63.vps.ovh.net:8085'
   // //'http://vps-2cf1715a.vps.ovh.net:8085'
 
 };
