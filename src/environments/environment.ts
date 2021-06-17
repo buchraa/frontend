8893dc9ec82b36b4018b36fdecfcf07b21557fd9
buchraa <ndiayebouchraa@gmail.com>
2021-06-17 18:00:48 +0200

    fix css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://bibliotechmouride.herokuapp.com' //'http://vps-8aea0a63.vps.ovh.net:8085'
-  // //'http://vps-2cf1715a.vps.ovh.net:8085'
-
+  apiUrl: 'http://vps-8aea0a63.vps.ovh.net:8085' 
 };
 /*
  * For easier debugging in development mode, you can import the following file
