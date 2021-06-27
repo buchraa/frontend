74f1c8ba6d60045b43d4318707e1278df9592321
buchraa <ndiayebouchraa@gmail.com>
2021-06-27 13:05:46 +0200

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:   'https://bibliotechmouride.herokuapp.com' // 'http://vps-8aea0a63.vps.ovh.net:8085'  
+  apiUrl: 'http://vps-8aea0a63.vps.ovh.net:8085'  
  
 };
